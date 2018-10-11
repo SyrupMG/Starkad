a15b7772c02bceecfc0f105ff7a8160a814bbbea
Davit Karapetian <karapetiandav@gmail.com>
2018-10-11 12:16:02 +0300

    settings docs

--- downloadservicelibrary/src/main/java/ru/ctcmedia/downloadservicelibrary/downloadservice/settings/Settings.kt
+++ downloadservicelibrary/src/main/java/ru/ctcmedia/downloadservicelibrary/downloadservice/settings/Settings.kt
@@ -5,11 +5,21 @@ import com.tonyodev.fetch2.NetworkType.WIFI_ONLY
 import ru.ctcmedia.downloadservicelibrary.downloadservice.settings.NetworkType.Cellular
 import ru.ctcmedia.downloadservicelibrary.downloadservice.settings.NetworkType.Wifi
 
+/*
+* Класс-структура который описывает:
+ * Кол-во возможных одновременных закачек
+ * Тип сети по которому возможно скачивание
+* */
 data class Settings(
     val concurrentDownloads: Int = 1,
     val networkType: NetworkType = Cellular
 )
 
+/*
+* Тип сети:
+* Cellular - мобильная и Wi-Fi
+* Wifi- только Wi-Fi
+* */
 enum class NetworkType {
     Wifi,
     Cellular;
