28a51754d0cf9b6ef5aaa5e020ea3c827a454ab0
Davit Karapetian <karapetiandav@gmail.com>
2018-10-11 12:15:47 +0300

    status listener docs

--- downloadservicelibrary/src/main/java/ru/ctcmedia/downloadservicelibrary/downloadservice/interfaces/DownloadServiceListener.kt
+++ downloadservicelibrary/src/main/java/ru/ctcmedia/downloadservicelibrary/downloadservice/interfaces/DownloadServiceListener.kt
@@ -1,10 +1,26 @@
 package ru.ctcmedia.downloadservicelibrary.downloadservice.interfaces
 
-
-
+/*
+* Интерфейс описывающий события которые могут порождать классы наследующие Downloadable
+* */
 interface DownloadStatusListener {
+    /*
+    * Метод вызывается когда объект начал скачиваться
+    * */
     fun downloadBegan() {}
+
+    /*
+    * Метод вызывается при изменении процента скачанного файла
+    * */
     fun downloadProgressUpdate(progress: Double) {}
+
+    /*
+    * Метод вызывается при окончании скачивания файла
+    * */
     fun downloadFinished() {}
+
+    /*
+    * Метод вызывается при ошибке скачивания файла
+    * */
     fun downloadFailed(error: Error) {}
 }
